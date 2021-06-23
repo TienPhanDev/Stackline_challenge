ab783d88e3eb2c86a8528ba10ed4fdbadfff5e5d
tienphandev <tienphan.dev@gmail.com>
2021-06-22 20:49:29 -0400

    Cleaned footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <div className="bg-gray-900">
+    <div className="bg-gray-500">
       <footer className="flex flex-wrap items-center justify-between m-auto">
         <div className="relative flex justify-center border-t-2 mx-auto text-white text-center">
-          Stackline © Tien Phan 2021
+          <a href="https://github.com/TienPhanDev/Stackline_challenge">Link to Repo Tien Phan 2021</a>
         </div>
       </footer>
     </div>
